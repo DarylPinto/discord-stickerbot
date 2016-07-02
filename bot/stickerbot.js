553745fbc2961ced00e00ee2307da26e6ac67770
DarylPinto <daryl.j.pinto@gmail.com>
2016-07-02 16:54:10 -0400

    Initial commit

---
+++ bot/stickerbot.js
@@ -0,0 +1,59 @@
+var Discord = require('discord.js');
+var request = require('request');
+var credentials = require('./login-info.json');
+
+var bot = new Discord.Client();
+var commandSymbol = ':';
+var triggerMessage;
+var stickers;
+
+function readStickerDB(error, response, body){
+
+	if(error) console.log(error);
+	if (!error && response.statusCode === 200){
+		//Store JSON file object
+		stickers = body;
+	}
+	postSticker();
+
+}
+
+function postSticker(){
+
+	var message = triggerMessage;
+
+	var stickerKey = message.content.slice(1, message.content.length - 1);
+	if(typeof stickers[stickerKey] === 'string'){
+
+		//Delete the message that triggered the response
+		bot.deleteMessage(message);
+		//Change nickname to match user who posted the message
+		bot.setNickname(message, message.sender.username, function(){
+			//Post sticker
+			bot.sendMessage(message, stickers[stickerKey], function(){
+				//Change nickname back to stickerbot
+				bot.setNickname(message, 'stickerbot');
+			});
+		});
+	}
+
+}
+
+bot.on('message', function(message){
+	//If first and last characters are `commandSymbol`
+	if(message.content[0] === commandSymbol &&
+		 message.content[message.content.length - 1] === commandSymbol ){
+		
+		//Save message as global variable for use in another function
+		triggerMessage = message;
+
+		//Check JSON file online for stickers
+		request({
+			url: 'http://darylpinto.com/stickerbot/stickers.json?nocache=' + (new Date()).getTime(),
+			json: true
+		}, readStickerDB);
+
+	}
+});
+
+bot.login(credentials.email, credentials.password);
