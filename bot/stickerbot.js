7be2ff033d5295b623329d2c8814c98c5a8c06ee
DarylPinto <daryl.j.pinto@gmail.com>
2016-07-11 00:17:08 -0400

    Add sticker response time

--- bot/stickerbot.js
+++ bot/stickerbot.js
@@ -25,7 +25,6 @@ function download(uri, filename, callback){
   request.head(uri, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
-    console.log('\n');
 
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
@@ -71,10 +70,13 @@ function respondTo(triggerMessage){
 			var filepath = 'stickercache/' + stickerKey + '-' + (new Date()).getTime() + '.png';
 
 			//Delete the message that triggered the response
+			console.time('Sticker Response Time');
 			bot.deleteMessage(triggerMessage);
 			//Post sticker
 			download(stickers[stickerKey], filepath, function(){
-			  bot.sendFile(triggerMessage, filepath, filepath, '**' + getAuthorDisplayName(triggerMessage) + ':**', () => fs.unlink(filepath) );
+			  bot.sendFile(triggerMessage, filepath, filepath, '**' + getAuthorDisplayName(triggerMessage) + ':**', () => fs.unlink(filepath, () => {
+			  	console.timeEnd('Sticker Response Time'); console.log('\n')
+			  }) );
 			});
 		}
 		
